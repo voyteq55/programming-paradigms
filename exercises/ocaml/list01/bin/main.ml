a3b7430fc9a095d1b14c4e7e04feec4e5d329ff7
voyteq55 <53490891+voyteq55@users.noreply.github.com>
2023-10-11 18:28:51 +0200

    Solve ex. 1 and 2 from list 1 in OCaml

--- exercises/ocaml/list01/bin/main.ml
+++ exercises/ocaml/list01/bin/main.ml
@@ -1,1 +1,15 @@
-let () = print_endline "Hello, World!"
+open Printf
+
+let rec flatten l =
+  match l with
+  | [] -> []
+  | h :: t -> h @ flatten t;;
+
+let rec count element my_list =
+  match my_list with
+  | [] -> 0
+  | x :: tail -> (if x = element then 1 else 0) + count element tail;;
+
+let () = List.iter (printf "%d ") (flatten [[1; 2]; [3; 4]])
+let () = List.iter (printf "%d ") (flatten [[];[];[1];[2; 3; 5]])
+let () = print_int (count "a" ["1"; "a"; "234"; "aaa"; "a"])
