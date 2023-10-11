22bce18eaa69498de4608f44e9d5cd1f5c42371c
voyteq55 <53490891+voyteq55@users.noreply.github.com>
2023-10-11 18:31:46 +0200

    Modify solution to ex. 2 with tuple as argument

--- exercises/ocaml/list01/bin/main.ml
+++ exercises/ocaml/list01/bin/main.ml
@@ -5,11 +5,11 @@ let rec flatten l =
   | [] -> []
   | h :: t -> h @ flatten t;;
 
-let rec count element my_list =
+let rec count (element, my_list) =
   match my_list with
   | [] -> 0
-  | x :: tail -> (if x = element then 1 else 0) + count element tail;;
+  | x :: tail -> (if x = element then 1 else 0) + count (element, tail);;
 
 let () = List.iter (printf "%d ") (flatten [[1; 2]; [3; 4]])
 let () = List.iter (printf "%d ") (flatten [[];[];[1];[2; 3; 5]])
-let () = print_int (count "a" ["1"; "a"; "234"; "aaa"; "a"])
+let () = print_int (count ("a", ["1"; "a"; "234"; "aaa"; "a"]))
