4f244da29f57205e6331639e3f7b37f2f096b386
voyteq55 <53490891+voyteq55@users.noreply.github.com>
2023-10-16 09:24:49 +0200

    Solve ex. 2 from list 1 in Scala

--- exercises/scala/list01/src/main/scala/Main.scala
+++ exercises/scala/list01/src/main/scala/Main.scala
@@ -1,5 +1,8 @@
 @main def hello: Unit =
-  println("Hello world!")
-  println(msg)
-
-def msg = "I was compiled by Scala 3. :)"
+  println(count("ABC", List("ABFC", "ABC", "AAAA", "", "ABC")))
+  println(count("", List()))
+  
+def count[A](x:A, xs: List[A]): Int =
+  if xs.length == 0 then 0
+  else if xs.head == x then (1 + count(x, xs.tail))
+  else count(x, xs.tail)
