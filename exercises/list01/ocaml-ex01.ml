58cd52a7c3ac0a007c2ff15581cc56fd926abf7d
voyteq55 <53490891+voyteq55@users.noreply.github.com>
2023-10-21 15:13:17 +0200

    Solve ex. 6 from list01 in OCaml

--- exercises/list01/ocaml-ex01.ml
+++ exercises/list01/ocaml-ex01.ml
@@ -14,6 +14,10 @@ let rec sqrList list =
   if list = [] then [] else
     let x = List.hd list in x * x :: sqrList (List.tl list)
 
+let rec listLength list =
+  if list = [] then 0 else
+    1 + listLength (List.tl list)
+
 let test1_1 = flatten [] = []
 let test1_2 = flatten [[]; []] = []
 let test1_3 = flatten [[1; 2]; [3; 4]] = [1; 2; 3; 4]
@@ -31,3 +35,8 @@ let test4_1 = sqrList [] = []
 let test4_2 = sqrList [1; 2; 3; -4] = [1; 4; 9; 16]
 let test4_3 = sqrList [0; -1; -2; -3] = [0; 1; 4; 9]
 let test4_4 = sqrList [5; 0; 0; 7; 7] = [25; 0; 0; 49; 49]
+
+let test6_1 = listLength [] = 0
+let test6_2 = listLength [[[]]; []; []; []] = 4
+let test6_3 = listLength [100; 200; 300; 400; 500] = 5
+let test6_4 = listLength ["a"; "b"; "c"] = 3
