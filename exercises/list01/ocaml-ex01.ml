7b33be5542d1bf672660b72d53f65966585e0f5b
voyteq55 <53490891+voyteq55@users.noreply.github.com>
2023-10-21 14:54:54 +0200

    Solve ex.4 from list01 in OCaml

--- exercises/list01/ocaml-ex01.ml
+++ exercises/list01/ocaml-ex01.ml
@@ -10,6 +10,10 @@ let rec replicate (x, n) =
   if n <= 0 then [] else
     x :: replicate (x, n - 1)
 
+let rec sqrList list =
+  if list = [] then [] else
+    let x = List.hd list in x * x :: sqrList (List.tl list)
+
 let test1_1 = flatten [] = []
 let test1_2 = flatten [[]; []] = []
 let test1_3 = flatten [[1; 2]; [3; 4]] = [1; 2; 3; 4]
@@ -22,3 +26,8 @@ let test2_3 = count ("a", ["a"; "A"; "a"; "aaaaaaa"; "b"; "a"]) = 3
 let test3_1 = replicate ("aa", 5) = ["aa"; "aa"; "aa"; "aa"; "aa"]
 let test3_2 = replicate ([], 2) = [[]; []]
 let test3_3 = replicate ("aaaaa", 0) = []
+
+let test4_1 = sqrList [] = []
+let test4_2 = sqrList [1; 2; 3; -4] = [1; 4; 9; 16]
+let test4_3 = sqrList [0; -1; -2; -3] = [0; 1; 4; 9]
+let test4_4 = sqrList [5; 0; 0; 7; 7] = [25; 0; 0; 49; 49]
