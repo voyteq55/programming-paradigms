024181fe42a4ff02610e01de240a1bd545446e34
voyteq55 <53490891+voyteq55@users.noreply.github.com>
2023-10-22 14:07:52 +0200

    Rewrite ex. 2 from list01 in Scala with proper tests

--- exercises/list01/scala-ex01.scala
+++ exercises/list01/scala-ex01.scala
@@ -1,8 +1,14 @@
-@main def hello: Unit =
-  println(count("ABC", List("ABFC", "ABC", "AAAA", "", "ABC")))
-  println(count("", List()))
-  
 def count[A](x:A, xs: List[A]): Int =
-  if xs.length == 0 then 0
-  else if xs.head == x then (1 + count(x, xs.tail))
-  else count(x, xs.tail)
+  if xs.length == 0 then 0 else
+     if xs.head == x then (1 + count(x, xs.tail)) else count(x, xs.tail)
+
+@main def ex01: Unit =
+  def assertEquals[T](actualResult: T, expectedResult: T) = {
+    if actualResult == expectedResult then print(s"TEST PASSED! ") else print(s"!!! TEST FAILED !!! ")
+    println(s"expected result: $expectedResult, actual result: $actualResult")
+  }
+
+  println("TESTS:")
+
+  assertEquals(count(3, List(1, 2, 3, 4)), 1)
+  assertEquals(count(List(List()), List(List(), List(), List(List(List())), List(List()), List(List()))), 2)
