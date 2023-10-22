b9f1404bd160ddcd3dfc8bf824a18e0a639dd9c3
voyteq55 <53490891+voyteq55@users.noreply.github.com>
2023-10-22 14:31:09 +0200

    Standardize function call syntax (Lab list01)

--- labs/lab01/scala-list01.scala
+++ labs/lab01/scala-list01.scala
@@ -9,7 +9,7 @@ def sumProd(s: Int, e: Int): (Int, Int) =
 def sumOfDivisors(n: Int, currentDivisor: Int): Int =
     if n <= 1 then 0 else
     if currentDivisor * currentDivisor > n then 1 else // adding 1 because dividing starts from 2, every number is divisible by 1
-        sumOfDivisors (n, currentDivisor + 1) +
+        sumOfDivisors(n, currentDivisor + 1) +
         (if n % currentDivisor != 0 then 0 else 
             currentDivisor + (if currentDivisor * currentDivisor < n then n / currentDivisor else 0))
 
@@ -18,7 +18,7 @@ def isPerfect(n: Int) = if n == 0 then false else n == sumOfDivisors(n, 2)
 def insert[T](list: List[T], element: T, index: Int): List[T] =
     if list == List() then List(element) else
         if index <= 0 then element :: list else
-            list.head :: insert (list.tail, element, index - 1)
+            list.head :: insert(list.tail, element, index - 1)
 
 @main def main = {
     def assertEquals[T](actualResult: T, expectedResult: T) = {
@@ -28,13 +28,13 @@ def insert[T](list: List[T], element: T, index: Int): List[T] =
 
     println("TESTS:")
 
-    assertEquals(reverse4 (1, 2, 3, 4), (4, 3, 2, 1))
-    assertEquals(reverse4 ("a", (), 5, List(0)), (List(0), 5, (), "a"))
+    assertEquals(reverse4(1, 2, 3, 4), (4, 3, 2, 1))
+    assertEquals(reverse4("a", (), 5, List(0)), (List(0), 5, (), "a"))
 
     assertEquals(sumProd(1, 7), (21, 720))
-    assertEquals(sumProd (-5, -1), (-14, 120))
-    assertEquals(sumProd (5, 2), (0, 1))
-    assertEquals(sumProd (-5, 6), (0, 0))
+    assertEquals(sumProd(-5, -1), (-14, 120))
+    assertEquals(sumProd(5, 2), (0, 1))
+    assertEquals(sumProd(-5, 6), (0, 0))
 
     assertEquals(isPerfect(1), false)
     assertEquals(isPerfect(2), false)
