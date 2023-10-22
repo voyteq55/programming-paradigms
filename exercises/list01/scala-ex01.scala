36c4127f9396aae1f8742fce9d9d61ec917ff066
voyteq55 <53490891+voyteq55@users.noreply.github.com>
2023-10-22 14:21:46 +0200

    Solve ex.1 from list01 in Scala

--- exercises/list01/scala-ex01.scala
+++ exercises/list01/scala-ex01.scala
@@ -1,3 +1,7 @@
+def flatten[A](xss: List[List[A]]): List[A] =
+  if xss.isEmpty then List() else
+    xss.head ++ flatten (xss.tail)
+
 def count[A](x:A, xs: List[A]): Int =
   if xs.length == 0 then 0 else
      if xs.head == x then (1 + count(x, xs.tail)) else count(x, xs.tail)
@@ -10,5 +14,11 @@ def count[A](x:A, xs: List[A]): Int =
 
   println("TESTS:")
 
+  assertEquals(flatten(List()), List())
+  assertEquals(flatten(List(List(), List())), List())
+  assertEquals(flatten(List(List(1, 2), List(3, 4))), List(1, 2, 3, 4))
+  assertEquals(flatten(List(List(), List(), List(1), List(2, 3, 5))), List(1, 2, 3, 5))
+
   assertEquals(count(3, List(1, 2, 3, 4)), 1)
   assertEquals(count(List(List()), List(List(), List(), List(List(List())), List(List()), List(List()))), 2)
+  assertEquals(count("a", List("a", "A", "a", "aaaaaaa", "b", "a")), 3)
