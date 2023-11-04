8fb0b0c4755a8c1b34ebdb3fabc0d24bfdf6db91
voyteq55 <53490891+voyteq55@users.noreply.github.com>
2023-11-04 21:25:36 +0100

    Solve ex.2 (split2) from lab02 in OCaml

--- labs/lab02/ocaml-list02.ml
+++ labs/lab02/ocaml-list02.ml
@@ -9,6 +9,26 @@ let cutAndMend a b listToCut =
     in cutAndMendRec (listToCut, 0)
   | false -> []
 
+let rec split2Rec listToSplit =
+  match listToSplit with
+  | h1 :: h2 :: t -> let (t1, t2) = split2Rec t in (h1 :: t1, h2 :: t2)
+  | h :: [] -> ([h], [])
+  | [] -> ([], [])
+
+let split2Tail listToSplit =
+  let rec reverse listToReverse =
+    let rec reverseInner (l, acc) =
+      match l with
+      | [] -> acc
+      | h :: t -> reverseInner (t, h :: acc)
+    in reverseInner (listToReverse, [])
+  in 
+  let rec split2TailRecursive (originalList, listAcc1, listAcc2) =
+    match originalList with
+    | [] -> (reverse listAcc1, reverse listAcc2)
+    | h :: t -> split2TailRecursive(t, h :: listAcc2, listAcc1)
+  in split2TailRecursive (listToSplit, [], [])
+
 let cutAndMend15 = cutAndMend 1 5
 let test1_1 = cutAndMend15 [0; 1; 2; 3; 4; 5; 6; 7]
 let test1_2 = cutAndMend15 [0; 1; 2; 3; 4]
@@ -38,3 +58,14 @@ let test1_13 = cutAndMend21 [0; 1; 2; 3; 4; 5; 6; 7]
 let test1_14 = cutAndMend21 [0; 1; 2; 3; 4]
 let test1_15 = cutAndMend21 [0]
 let test1_16 = cutAndMend21 []
+
+let test2_1 = split2Rec [0; 1; 2; 3; 4; 5; 6; 7; 8]
+let test2_2 = split2Tail [0; 1; 2; 3; 4; 5; 6; 7; 8]
+let test2_3 = split2Rec [0; 1; 2; 3; 4; 5; 6; 7]
+let test2_4 = split2Tail [0; 1; 2; 3; 4; 5; 6; 7]
+let test2_5 = split2Rec [0; 1]
+let test2_6 = split2Tail [0; 1]
+let test2_7 = split2Rec [0]
+let test2_8 = split2Tail [0]
+let test2_9 = split2Rec []
+let test2_10 = split2Tail []
