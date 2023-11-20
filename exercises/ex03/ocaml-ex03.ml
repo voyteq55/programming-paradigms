917fe08f41172e479e669111ebc478673fc2140c
voyteq55 <53490891+voyteq55@users.noreply.github.com>
2023-11-20 22:17:25 +0100

    (list03 ex. 2) Full (un)currying in OCaml

--- exercises/ex03/ocaml-ex03.ml
+++ exercises/ex03/ocaml-ex03.ml
@@ -2,5 +2,7 @@ let f1 x = x 2 2
 let f2 x y z = x (y ^ z)
 
 let curry3 f x y z = f (x, y, z)
+let fullCurry3 = fun f -> fun x -> fun y -> fun z -> f (x, y, z)
 
 let uncurry3 f (x, y, z) = f x y z
+let fullUncurry3 = fun f -> fun (x, y, z) -> f x y z
