30c26e73316be46f4e71a4cd60d88296c4da5e87
voyteq55 <53490891+voyteq55@users.noreply.github.com>
2023-12-11 12:41:40 +0100

    Implement lab05 modification

--- labs/lab05/ocaml-lab05.ml
+++ labs/lab05/ocaml-lab05.ml
@@ -25,3 +25,47 @@ let buyTicketTest4 = buyTicket buyTicketTest3 3
 
 let buyTicketTest5 = buyTicket tickets 0
 let buyTicketTest5 = buyTicket tickets 50
+
+type lotteryTicketMod = TicketMod of lotteryTicketMod Lazy.t | RewardMod of string
+
+let rec buyTicketMod tableWithTickets ticketNumber =
+  match tableWithTickets with
+  | [] -> []
+  | ticketsHead :: ticketsTail ->
+    if ticketNumber <> 1 then ticketsHead :: buyTicketMod ticketsTail (ticketNumber - 1) else
+      match ticketsHead with
+      | RewardMod(rewardName) -> RewardMod(rewardName) :: ticketsTail
+      | TicketMod(ticketToReveal) -> Lazy.force ticketToReveal :: ticketsTail
+
+let ticketsMod = [
+  TicketMod(lazy (RewardMod("reward1")));
+  TicketMod(lazy (RewardMod("reward2")));
+  TicketMod(lazy (TicketMod(lazy (RewardMod("reward3")))));
+  TicketMod(lazy (TicketMod(lazy (RewardMod("reward4")))));
+  TicketMod(lazy (RewardMod("reward5")))
+]
+
+let ticketsMod2 = [
+  TicketMod(lazy (RewardMod("reward1")));
+  TicketMod(lazy (RewardMod("reward2")));
+  TicketMod(lazy (TicketMod(lazy (RewardMod("reward3")))));
+  TicketMod(lazy (TicketMod(lazy (RewardMod("reward4")))));
+  TicketMod(lazy (RewardMod("reward5")))
+]
+
+let ticketsMod3 = [
+  TicketMod(lazy (RewardMod("reward1")));
+  TicketMod(lazy (RewardMod("reward2")));
+  TicketMod(lazy (TicketMod(lazy (RewardMod("reward3")))));
+  TicketMod(lazy (TicketMod(lazy (RewardMod("reward4")))));
+  TicketMod(lazy (RewardMod("reward5")))
+]
+
+let buyTicketModTest1 = buyTicketMod ticketsMod 2
+let buyTicketModTest2 = buyTicketMod buyTicketModTest1 2
+
+let buyTicketModTest3 = buyTicketMod ticketsMod2 3
+let buyTicketModTest4 = buyTicketMod buyTicketModTest3 3
+
+let buyTicketModTest5 = buyTicketMod ticketsMod3 0
+let buyTicketModTest6 = buyTicketMod ticketsMod3 50
