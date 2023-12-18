b655f4e6a73167574ccf2f6867290cbc50e0d33b
voyteq55 <53490891+voyteq55@users.noreply.github.com>
2023-12-18 12:30:01 +0100

    (lab06) Write modification (composites) in OCaml

--- labs/lab06/ocaml-lab06.ml
+++ labs/lab06/ocaml-lab06.ml
@@ -23,3 +23,41 @@ let testImperacci2 = imperacci 2 6
 let testImperacci3 = imperacci 4 10
 let testImperacci4 = imperacci 2 0
 let testImperacci5 = imperacci 0 6
+
+let composites n =
+  let
+    isPrime = Array.make (n + 1) true and
+    index = ref 2
+  in
+    while !index * !index <= n do
+      if isPrime.(!index) then 
+        let multiple = ref (2 * !index) in
+        while !multiple <= n do
+          isPrime.(!multiple) <- false;
+          multiple := !multiple + !index
+        done;
+      ;
+      index := !index + 1
+    done;
+    index := 0;
+    let numberOfComposites = ref 0 in
+    while !index <= n do
+      if not isPrime.(!index) then
+        numberOfComposites := !numberOfComposites + 1;
+      index := !index + 1
+    done;
+    index := 0;
+    let compositeNumbers = Array.make !numberOfComposites 0 and
+    compositesIndex = ref 0
+    in
+    while (!index <= n) do
+      if not isPrime.(!index) then (
+        compositeNumbers.(!compositesIndex) <- !index;
+        compositesIndex := !compositesIndex + 1
+      );
+      index := !index + 1
+    done;
+    compositeNumbers
+
+let testComposite1 = composites 0
+let testComposite2 = composites 121
